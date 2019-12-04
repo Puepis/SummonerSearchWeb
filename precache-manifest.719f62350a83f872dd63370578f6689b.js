b503b1882ef2bd33549418d4ceb260c5db6633a6
Philips Xu <philips.ilips@gmail.com>
2019-12-04 16:51:39 -0500

    Updates

---
+++ precache-manifest.719f62350a83f872dd63370578f6689b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cbd53ffb3f9c6a02cfa802bc7f6292",
+    "url": "/lolsearcher/index.html"
+  },
+  {
+    "revision": "f5675c531f5ca4af2fa1",
+    "url": "/lolsearcher/static/css/main.5744efd8.chunk.css"
+  },
+  {
+    "revision": "f7a97d92b636190e22b1",
+    "url": "/lolsearcher/static/js/2.e9a456ee.chunk.js"
+  },
+  {
+    "revision": "f5675c531f5ca4af2fa1",
+    "url": "/lolsearcher/static/js/main.f576b577.chunk.js"
+  },
+  {
+    "revision": "c8e1425259888d014fbb",
+    "url": "/lolsearcher/static/js/runtime-main.63c81fc7.js"
+  },
+  {
+    "revision": "3ad17a20a22b8b4a8a541c255c7ba127",
+    "url": "/lolsearcher/static/media/bronze.3ad17a20.png"
+  },
+  {
+    "revision": "48f98701c8589f4656c889fcb39d2b59",
+    "url": "/lolsearcher/static/media/challenger.48f98701.png"
+  },
+  {
+    "revision": "55c497d777e6d0f37a2fc47f10823d01",
+    "url": "/lolsearcher/static/media/diamond.55c497d7.png"
+  },
+  {
+    "revision": "b787163cca62a75d3e9d16d81115f667",
+    "url": "/lolsearcher/static/media/gold.b787163c.png"
+  },
+  {
+    "revision": "6a35ceeafb68e3f3ea081cef61fbfe6c",
+    "url": "/lolsearcher/static/media/grandmaster.6a35ceea.png"
+  },
+  {
+    "revision": "d283cf7a42111c7a5a61377caf18569e",
+    "url": "/lolsearcher/static/media/iron.d283cf7a.png"
+  },
+  {
+    "revision": "1169f44704e5c28aec109244daa4568a",
+    "url": "/lolsearcher/static/media/lol_icon.1169f447.ico"
+  },
+  {
+    "revision": "4927dbc288ebf7b1b6b0556b74015e67",
+    "url": "/lolsearcher/static/media/lol_icon.4927dbc2.png"
+  },
+  {
+    "revision": "fad9106986ec7bbd744b9ef53b3c4814",
+    "url": "/lolsearcher/static/media/ls_icon.fad91069.ico"
+  },
+  {
+    "revision": "1b77ca936792dcfb9b121f5b1322ed4d",
+    "url": "/lolsearcher/static/media/master.1b77ca93.png"
+  },
+  {
+    "revision": "32742990acfd0509f5b8c50851e31d63",
+    "url": "/lolsearcher/static/media/platinum.32742990.png"
+  },
+  {
+    "revision": "3ad0652bd17ff826a31fa29366021cfd",
+    "url": "/lolsearcher/static/media/robots.3ad0652b.txt"
+  },
+  {
+    "revision": "61992f040bb8b6b052dce01afeba4a70",
+    "url": "/lolsearcher/static/media/silver.61992f04.png"
+  },
+  {
+    "revision": "c428b2ae75826b6e6ff5438faebdffe4",
+    "url": "/lolsearcher/static/media/unranked.c428b2ae.png"
+  }
+]);
