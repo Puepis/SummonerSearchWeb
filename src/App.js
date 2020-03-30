c1d324fd841f1b7048177f91b2a096fb1cbd438c
Philips Xu <philips.ilips@gmail.com>
2020-03-29 23:01:32 -0400

    test

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
         try {
 
             const summonerIDResponse = await
-                axios.get("http://localhost:3000/lol/summoner/v4/summoners/by-name/Maplecoolz?api_key=RGAPI-92a7f330-c5a0-4d91-a2bf-7116fc886806");
+                axios.get("/lol/summoner/v4/summoners/by-name/Maplecoolz?api_key=RGAPI-92a7f330-c5a0-4d91-a2bf-7116fc886806");
 
             // Basic summoner info (profile pic id, name, level)
             /*const summonerIDResponse = await
